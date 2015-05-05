18a30de21bcf7cd3725a32d5af800c2c10fc5f56
vaibhavsagar <vaibhavsagar@gmail.com>
2015-05-05 22:23:12 +1000

    Verify existence of `curl`.

--- .ci/prepare.sh
+++ .ci/prepare.sh
@@ -1,3 +1,4 @@
+curl
 # Get the latest .deb released.
 wget `curl https://api.github.com/repos/jgm/pandoc/releases/latest | jq -r '.assets[] | .browser_download_url | select(endswith("deb"))'` -O pandoc.deb
 dpkg -i pandoc.deb
