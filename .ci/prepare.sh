f8550977816d009889f7d918db1989838b121f46
vaibhavsagar <vaibhavsagar@gmail.com>
2015-05-05 22:19:16 +1000

    Try dpkg without sudo.

--- .ci/prepare.sh
+++ .ci/prepare.sh
@@ -1,3 +1,3 @@
 # Get the latest .deb released.
 wget `curl https://api.github.com/repos/jgm/pandoc/releases/latest | jq -r '.assets[] | .browser_download_url | select(endswith("deb"))'` -O pandoc.deb
-sudo dpkg -i pandoc.deb
+dpkg -i pandoc.deb
