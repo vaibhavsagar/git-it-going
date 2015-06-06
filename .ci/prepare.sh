b2a2970b0375af6dbc3b61d97f0197c857146361
vaibhavsagar <vaibhavsagar@gmail.com>
2015-06-06 10:02:21 +1000

    Check if pandoc_url is set.

--- .ci/prepare.sh
+++ .ci/prepare.sh
@@ -1,6 +1,8 @@
 # Install jq to filter Github release data for Pandoc.
 sudo apt-get -y install jq
 jq --version
+PANDOC_URL=`curl https://api.github.com/repos/jgm/pandoc/releases/latest | jq -r '.assets[] | .browser_download_url | select(endswith("deb"))'`
+echo $PANDOC_URL
 # Get the latest .deb released.
 wget `curl https://api.github.com/repos/jgm/pandoc/releases/latest | jq -r '.assets[] | .browser_download_url | select(endswith("deb"))'` -O pandoc.deb
 sudo dpkg -i pandoc.deb
